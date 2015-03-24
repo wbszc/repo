4e69e3c9b48db542b5d272d18be0d98d7db795ba
ChaoZhang <zhangchao1023@gmail.com>
2015-03-23 22:05:35 -0700

    Add 'TwoSum '

--- leetcode/java/array/ArrayCode.java
+++ leetcode/java/array/ArrayCode.java
@@ -181,5 +181,25 @@ Suppose a sorted array is rotated at some pivot unknown to you beforehand. (i.e.
         }
         return max;
     }
+    
+    /**
+     Given an array of integers, find two numbers such that they add up to a specific target number. The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based. You may assume that each input would have exactly one solution. Input: numbers={2, 7, 11, 15}, target=9.Output: index1=1, index2=2
+     */
+    public static int[] findTwoSum(int[] arr, int tar){
+        if(arr == null || arr.length == 0) return null;
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int[] res =new int[2];
+        for(int i = 0; i < arr.length; i ++){
+            if(!map.containsKey(arr[i])){
+                map.put(tar - arr[i], i+1);
+            }else{
+                //found it
+                res[0] = map.get(arr[i]);
+                res[1] = i+1;
+                break;
+            }
+        }
+        return res;
+    }
 
 }
