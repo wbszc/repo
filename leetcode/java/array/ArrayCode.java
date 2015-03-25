139dd562890eabb343e02333088a37c32da50818
ChaoZhang <zhangchao1023@gmail.com>
2015-03-24 17:47:36 -0700

    Three sum added

--- leetcode/java/array/ArrayCode.java
+++ leetcode/java/array/ArrayCode.java
@@ -202,4 +202,12 @@ Suppose a sorted array is rotated at some pivot unknown to you beforehand. (i.e.
         return res;
     }
 
+    /**
+     Given an array S of n integers, are there elements a,b,c in S such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero. Note:• Elements in a triplet (a, b, c) must be in non-descending order. (ie, a ≤ b ≤ c) • The solution set must not contain duplicate triplets.
+     For example, given array S = {-1 0 1 2 -1 -4}. A solution set is: (-1, 0, 1) (-1, -1, 2)
+     */
+    public static ArrayList<ArrayList<Integer>> findThreeSum(int[] arr, int tar){
+        
+        return null;
+    }
 }
