31fc97fb70518a1536d37ab830a7d9937c9bbc24
ChaoZhang <zhangchao1023@gmail.com>
2015-03-28 00:16:44 -0400

    start one test

--- leetcode/java/array/ArrayCode.java
+++ leetcode/java/array/ArrayCode.java
@@ -389,4 +389,11 @@ Suppose a sorted array is rotated at some pivot unknown to you beforehand. (i.e.
         }
         return;
     }
+    
+    /**
+     The set [1,2,3,⋯,n] contains a total of n! unique permutations.By listing and labeling all of the permutations in order, we get the following sequence (ie, for n = 3): "123"  "132" "213 "231" "312" "321"      Given n and k, return the kth permutation sequence. Note: Given n will be between 1 and 9 inclusive.
+     */
+    public void findSpecialPermutations(){
+        
+    }
 }
