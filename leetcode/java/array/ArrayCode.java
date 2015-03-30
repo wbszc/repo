fcd8fbe2ce3e0b575816805e39717a60a55d6eb0
ChaoZhang <zhangchao1023@gmail.com>
2015-03-29 22:23:52 -0400

    make one

--- leetcode/java/array/ArrayCode.java
+++ leetcode/java/array/ArrayCode.java
@@ -148,6 +148,23 @@ Suppose a sorted array is rotated at some pivot unknown to you beforehand. (i.e.
      There are two sorted arrays A and B of size m and n respectively. Find the median of the two sorted arrays. The overall run time complexity should be O(log(m + n)).
      */
     public static double medianOfTwoSortedArray(int[] arr1, int[] arr2){
+        if(arr1 == null || arr2 == null) return arr1 == null? getIt(arr2) : getIt(arr1);
+        if((arr1.length + arr2.length) %2 == 1) {
+            return findK(arr1, 0, arr1.length-1, arr2, 0, arr2.length-1, (arr1.length + arr2.length)/2);
+        }else{
+            return (findK(arr1, 0, arr1.length-1, arr2, 0, arr2.length-1, (arr1.length + arr2.length)/2-1) + findK(arr1, 0, arr1.length-1, arr2, 0, arr2.length-1, (arr1.length + arr2.length)/2))/2;
+        }
+    }
+    
+    public static double getIt(int[] arr){
+        if(arr == null) return 0.0;
+        else {
+            if(arr.length %2 == 1) return arr[arr.length/2];
+            else return (arr[arr.length/2-1] + arr[arr.length/2])/2;
+        }
+    }
+    
+    public static double findK(int[] arrA, int aStart, int aEnd, int[] arrB, int bStart, int bEnd, int k){
         return 0.0;
     }
     
@@ -401,7 +418,7 @@ Suppose a sorted array is rotated at some pivot unknown to you beforehand. (i.e.
      Given a number represented as an array of digits, plus one to the number.
      */
     public static int[] addOne(int[] arr){
-        
+        return null;
         
     }
 }
