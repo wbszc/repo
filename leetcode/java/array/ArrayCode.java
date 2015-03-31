84183f02770bea41a009279b056e746438474679
ChaoZhang <zhangchao1023@gmail.com>
2015-03-30 23:15:01 -0400

    Find special permutation

--- leetcode/java/array/ArrayCode.java
+++ leetcode/java/array/ArrayCode.java
@@ -420,8 +420,30 @@ Suppose a sorted array is rotated at some pivot unknown to you beforehand. (i.e.
     /**
      The set [1,2,3,⋯,n] contains a total of n! unique permutations.By listing and labeling all of the permutations in order, we get the following sequence (ie, for n = 3): "123"  "132" "213 "231" "312" "321"      Given n and k, return the kth permutation sequence. Note: Given n will be between 1 and 9 inclusive.
      */
-    public void findSpecialPermutations(){
-        
+    public String findSpecialPermutations(int n, int k){
+        if(n <= 0) return "";
+        k--;
+        StringBuilder sb = new StringBuilder();
+        int fact = 1;
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for(int i = 2; i <= n ; i++){
+            fact*=i;
+        }//now fact has the total number of n
+        for(int i = 1; i <= n; i++){
+            list.add(i);
+        }//now list contains all the rounds of the numbers
+        int round = n-1;
+        while(round >= 0){
+            int index = k/fact;
+            k%= fact;
+            sb.append(list.get(index));
+            list.remove(index);
+            if(round> 0){
+                fact /= round;
+            }
+            round --;
+        }
+        return sb.toString();
     }
     
     /**
