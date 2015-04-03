c310e9fb381e53cd72f7400917f37f37dc9a2f64
ChaoZhang <zhangchao1023@gmail.com>
2015-04-02 21:35:41 -0400

    climb stairs

--- leetcode/java/array/ArrayCode.java
+++ leetcode/java/array/ArrayCode.java
@@ -537,4 +537,21 @@ Suppose a sorted array is rotated at some pivot unknown to you beforehand. (i.e.
         }
         return arr;
     }
+
+    /**
+You are climbing a stair case. It takes n steps to reach to the top. Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+    */
+    public static int climbStairs(int n){
+	if(n<=0) return 0;
+	if(n==1) return 1;
+	int[]x = new int[n+1];
+	x[0]=0;
+	x[1]=1;
+	x[2]=2;
+	for(int i=3; i < n+1; i++){
+	    x[i]=x[i-1]+x[i-2];   
+	}
+	return x[n];
+    }
+
 }
